5f392b039972b10aab392e0f4cf71b7339827138
jsndz <jaisondz9360@gmail.com>
2025-02-18 23:38:55 +0530

    get from github

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import { PORT } from "./config";
+import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils/utils";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/deploy", (req, res) => {
+  const url = req.body.url;
+  const id = generate();
+  simpleGit().clone(url, `out/${id}`);
+  res.json({ id });
+});
 
 app.listen(PORT, () => {
   console.log(`sever running in ${PORT}`);
