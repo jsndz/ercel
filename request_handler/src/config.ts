ece36ebaa9893876da335334a643d712cc55536d
jsndz <jaisondz9360@gmail.com>
2025-02-22 15:50:51 +0530

    forward content

--- request_handler/src/config.ts
+++ request_handler/src/config.ts
@@ -3,3 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
+export const R2_BUCKET_ID = process.env.R2_BUCKET_ID;
+export const R2_BUCKET_SECRET = process.env.R2_BUCKET_SECRET;
+export const R2_BUCKET_ENDPOINT = process.env.R2_BUCKET_ENDPOINT;
--- request_handler/src/index.ts
+++ request_handler/src/index.ts
@@ -1,14 +1,38 @@
 import express from "express";
-import { PORT } from "./config";
+import {
+  PORT,
+  R2_BUCKET_ENDPOINT,
+  R2_BUCKET_ID,
+  R2_BUCKET_SECRET,
+} from "./config";
+import { S3 } from "aws-sdk";
+
+const s3 = new S3({
+  accessKeyId: R2_BUCKET_ID,
+  secretAccessKey: R2_BUCKET_SECRET,
+  endpoint: R2_BUCKET_ENDPOINT,
+});
 
 const app = express();
 
-app.get("/*", (req, res) => {
+app.get("/*", async (req, res) => {
   const hostname = req.hostname;
   console.log(hostname);
-
   const id = hostname.split(".")[0];
-  console.log(id);
+  const filePath = req.path;
+  const content = await s3
+    .getObject({
+      Bucket: "ercel",
+      Key: `/dist/${id}${filePath}`,
+    })
+    .promise();
+  const type = filePath.endsWith("html")
+    ? "text/html"
+    : filePath.endsWith("css")
+    ? "text/css"
+    : "application/javascript";
+  res.set("Content-type", type);
+  res.send(content.Body);
 });
 
 app.listen(PORT, () => {
